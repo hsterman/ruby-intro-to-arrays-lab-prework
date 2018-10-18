59b9558791cba81a11455f3f9a4f0a30b260052c
heather sterman <heather.sterman531@gmail.com>
2018-10-18 20:45:41 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,14 +3,14 @@ def instantiate_new_array
   return array
 end
 
-def array_with_two_elements
+def array_with_two_elements(object, object)
   array = []
   return array
 end
 
-def first_element
+def first_element(array)
   array = []
-  return array
+  return first_element
 end
 
 def last_element_with_array_methods
